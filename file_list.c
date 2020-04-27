9a2e48ed5f9a1c689d83c4e9a502780c4497603c
ArchieLiu0129 <detective413@gmail.com>
2020-04-27 21:27:25 +0800

    Add cat do_command.c file_list.c find_command.c ls noop number parse.c removetag removetag0 test.html

---
+++ file_list.c
@@ -0,0 +1,41 @@
+/*
+ * file_list.c: to list out all the file in the current direction
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include "shell.h"
+
+char **file_list(void){
+	DIR *d;
+	int count = 0;
+	char *name;
+	char **name_list;
+	name_list = malloc(100*sizeof(*name_list));
+	struct dirent *dir;
+	d = opendir(".");
+	if(d){
+		while((dir = readdir(d)) != NULL){
+			name = dir->d_name;
+			name_list[count] = malloc((strlen(name)+1)*sizeof(*name_list[count]));
+			strcpy(name_list[count], name);
+			count++;
+		}
+		closedir(d);
+	}
+	name_list[count] = '\0';
+	/*for(int i = 0 ; i < count ; i++){
+		printf("%s\n",name_list[i]);
+	}*/
+	return name_list;
+}
+
+void free_name_list(char **old_list){
+	int i = 0;
+	while(old_list[i]!=NULL){
+		old_list[i] = (char* )realloc(old_list[i], NULL);
+	}
+	old_list = (char** )realloc(old_list, NULL);
+}
