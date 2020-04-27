424906f17eb20ff786ca17be069e67114d630dbb
ArchieLiu0129 <34954039+ArchieLiu0129@users.noreply.github.com>
2020-04-28 00:05:20 +0800

    Delete shell.h

--- shell.h
+++
@@ -1,6 +0,0 @@
-char **parse(char *);
-void free_argv(char **);
-char **file_list(void);
-void free_name_list(char **);
-int find_command(const char **, const char *);
-void do_command(char **myArgv);
